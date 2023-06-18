7125d71293d55712bd7145e2cfb5c9ded698acd6
Cameron <cameron@cameronheard.com>
2023-06-18 18:14:53 -0500

    Updated main.go and config-generator.html
    main.go was updated to allow querying Microsoft Access databases.
    
    config-generator.html was updated to list Ham Radio Software instead of databases.

--- main.go
+++ main.go
@@ -2,120 +2,244 @@ package main
 
 import (
 	"database/sql"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/gorilla/feeds"
+	"github.com/bennof/accessDBwE"
+	"github.com/gohugoio/hugo/parser"
+	"github.com/gohugoio/hugo/parser/metadecoders"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type Post struct {
-	Title   string
-	Author  string
-	Content string
-	Date    time.Time
+type Configuration struct {
+	DBType          string `json:"dbType"`
+	Database        string `json:"database"`
+	Table           string `json:"table"`
+	ContentColumn   string `json:"contentColumn"`
+	TitleColumn     string `json:"titleColumn"`
+	Description     string `json:"description"`
+	Link            string `json:"link"`
+	Author          string `json:"author"`
+	Email           string `json:"email"`
+	RSSFileName     string `json:"rssFileName"`
+	MarkdownFolder  string `json:"markdownFolder"`
+	StatusRSSFile   string `json:"statusRSSFile"`
+	OnAirText       string `json:"onAirText"`
+	OffAirText      string `json:"offAirText"`
+	IntervalMinutes int    `json:"intervalMinutes"`
+	HugoPostTitle   string `json:"hugoPostTitle"`
+}
+
+type Item struct {
+	XMLName     xml.Name `xml:"item"`
+	Title       string   `xml:"title"`
+	Link        string   `xml:"link"`
+	Description string   `xml:"description"`
+	Author      string   `xml:"author"`
+	Category    string   `xml:"category"`
+	PubDate     string   `xml:"pubDate"`
+}
+
+type Channel struct {
+	XMLName     xml.Name `xml:"channel"`
+	Title       string   `xml:"title"`
+	Link        string   `xml:"link"`
+	Description string   `xml:"description"`
+	Items       []Item   `xml:"item"`
+}
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Version string   `xml:"version,attr"`
+	Channel Channel  `xml:"channel"`
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "./foo.db")
+	// Load Configuration
+	config := loadConfig("config.json")
+	// Open DB connection
+	var db *sql.DB
+	var err error
+	if config.DBType == "access" {
+		db, err = accessDBwE.Open("adodb", "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+config.Database)
+	} else if config.DBType == "sqlite" {
+		db, err = sql.Open("sqlite3", config.Database)
+	}
 	if err != nil {
-		fmt.Println("Failed to open database:", err)
-		return
+		log.Fatal(err)
 	}
 	defer db.Close()
 
-	row := db.QueryRow("SELECT title, author, content, date FROM posts ORDER BY date DESC LIMIT 1")
-
-	var post Post
-	err = row.Scan(&post.Title, &post.Author, &post.Content, &post.Date)
+	// Get Data from DB
+	rows, err := db.Query("SELECT * FROM " + config.Table)
 	if err != nil {
-		fmt.Println("Failed to read post:", err)
-		return
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Loop over DB rows, create Items and write to Hugo
+	var items []Item
+	for rows.Next() {
+		// Extract data from row
+		var id int
+		var content string
+		err = rows.Scan(&id, &content)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create an Item and append to items
+		item := Item{
+			Title:       fmt.Sprintf("%s %d", config.TitleColumn, id),
+			Link:        config.Link,
+			Description: content,
+			Author:      config.Author,
+			PubDate:     time.Now().Format(time.RFC822),
+		}
+		items = append(items, item)
+
+		// Create a Hugo post
+		writeToHugo(item, config.MarkdownFolder, config.HugoPostTitle)
+
+		// Wait for interval
+		time.Sleep(time.Duration(config.IntervalMinutes) * time.Minute)
+
+		// Check if next episode is ready
+		status := checkStatus(db, config.Table, config.ContentColumn)
+		writeStatusRSS(status, config)
+
+		// If on air, publish next episode
+		if status == config.OnAirText {
+			publishNextEpisode(db, config.Table, config.ContentColumn)
+		}
 	}
 
-	// Create a markdown file for the post
-	content := fmt.Sprintf(`---
-title: "%s"
-author: "%s"
-date: "%s"
----
-
-%s
-`, post.Title, post.Author, post.Date.Format(time.RFC3339), post.Content)
-
-	err = ioutil.WriteFile("post.md", []byte(content), 0644)
-	if err != nil {
-		fmt.Println("Failed to write post:", err)
-		return
-	}
-
-	// Create an RSS feed
-	feed := &feeds.Feed{
-		Title:       "My feed",
-		Link:        &feeds.Link{Href: "http://example.com"},
-		Description: "This is my personal feed!",
-		Author:      &feeds.Author{Name: "John Doe", Email: "johndoe@example.com"},
-		Created:     time.Now(),
-	}
-
-	feed.Items = []*feeds.Item{
-		{
-			Title:       post.Title,
-			Link:        &feeds.Link{Href: "http://example.com/" + post.Title},
-			Description: post.Content,
-			Author:      &feeds.Author{Name: post.Author},
-			Created:     post.Date,
+	// Write Items to RSS
+	rss := &RSS{
+		Version: "2.0",
+		Channel: Channel{
+			Title:       config.TitleColumn,
+			Link:        config.Link,
+			Description: config.Description,
+			Items:       items,
 		},
 	}
-
-	rss, err := feed.ToRss()
+	rssData, err := xml.MarshalIndent(rss, "", "  ")
 	if err != nil {
-		fmt.Println("Failed to generate RSS:", err)
-		return
+		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile("feed.rss", []byte(rss), 0644)
+	// Write RSS data to file
+	err = ioutil.WriteFile(config.RSSFileName, rssData, 0644)
 	if err != nil {
-		fmt.Println("Failed to write RSS:", err)
-		return
+		log.Fatal(err)
+	}
+}
+
+func writeToHugo(item Item, folder string, title string) {
+	// Create a new File
+	newpath := filepath.Join(folder, fmt.Sprintf("%s.md", item.Title))
+	newFile, err := os.Create(newpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer newFile.Close()
+
+	// Create a new Page
+	page, err := parser.NewPageParser().Parse(strings.NewReader(""))
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Create a second RSS feed for status
-	statusFeed := &feeds.Feed{
-		Title:       "Status feed",
-		Link:        &feeds.Link{Href: "http://example.com/status"},
-		Description: "This feed shows the status of the database.",
-		Author:      &feeds.Author{Name: "Status Bot", Email: "status@example.com"},
-		Created:     time.Now(),
+	// Set page metadata
+	page.FrontMatter["title"] = title
+	page.FrontMatter["date"] = time.Now().Format(time.RFC3339)
+	page.FrontMatter["draft"] = false
+
+	// Set page content
+	page.Content = item.Description
+
+	// Write to File
+	err = page.Render(newFile, metadecoders.FormatJSON)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkStatus(db *sql.DB, table string, contentColumn string) string {
+	rows, err := db.Query("SELECT " + contentColumn + " FROM " + table + " LIMIT 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		return "On"
+	} else {
+		return "Off"
+	}
+}
+
+func writeStatusRSS(status string, config Configuration) {
+	// Create a new status Item
+	item := Item{
+		Title:       "Status",
+		Link:        config.Link,
+		Description: status,
+		Author:      config.Author,
+		PubDate:     time.Now().Format(time.RFC822),
 	}
 
-	status := "On"
-	if time.Since(post.Date) > 30*time.Minute {
-		status = "Off"
-	}
-
-	statusFeed.Items = []*feeds.Item{
-		{
+	// Write status Item to RSS
+	rss := &RSS{
+		Version: "2.0",
+		Channel: Channel{
 			Title:       "Status",
-			Link:        &feeds.Link{Href: "http://example.com/status"},
-			Description: status,
-			Created:     time.Now(),
+			Link:        config.Link,
+			Description: "Status RSS",
+			Items:       []Item{item},
 		},
 	}
+	rssData, err := xml.MarshalIndent(rss, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	s
+	// Write RSS data to file
+	err = ioutil.WriteFile(config.StatusRSSFile, rssData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
+func publishNextEpisode(db *sql.DB, table string, contentColumn string) {
+	_, err := db.Exec("UPDATE " + table + " SET " + contentColumn + " = 'Off' WHERE "
+	contentColumn + " = 'On' LIMIT 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	statusRss, err := statusFeed.ToRss()
-    if err != nil {
-        fmt.Println("Failed to generate status RSS:", err)
-        return
-    }
+func main() {
+	// Load Configuration
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = ioutil.WriteFile("status.rss", []byte(statusRss), 0644)
-    if err != nil {
-        fmt.Println("Failed to write status RSS:", err)
-        return
-    }
+	// Open Database
+	db, err := accessdbwe.Open("adodb", "Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+config.DatabasePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create RSS Feed
+	createRSSFeed(db, config)
 }
